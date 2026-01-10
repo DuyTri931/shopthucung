fe256ba380e86ed0dfef6e3d52a0ca05d3b2e21d
TongLeDuyTri <tritong392@gmail.com>
2026-01-10 15:04:25 +0700

    Fix https assets + remove localhost link

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -38,6 +38,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+     if (app()->environment('production')) 
+        URL::forceScheme('https');
     }
 }
